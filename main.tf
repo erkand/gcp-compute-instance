e5b778764d29d6dd47df10bd6e6a9ad264759c85
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-06-17 13:51:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,18 +37,18 @@ variable "image" {
 
 provider "google" {
   #credentials = "${var.gcp_credentials}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
+  project     = var.gcp_project
+  region      = var.gcp_region
 }
 
 resource "google_compute_instance" "demo" {
-  name         = "${var.instance_name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.gcp_zone}"
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
 
   boot_disk {
     initialize_params {
-      image = "${var.image}"
+      image = var.image
     }
   }
 
@@ -63,5 +63,5 @@ resource "google_compute_instance" "demo" {
 }
 
 output "external_ip"{
-  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = google_compute_instance.demo.network_interface.0.access_config.0.nat_ip
 }
